9efb22dbff6175abc57d2b8728f223b8b9fed7c2
ChengJoee <540154940@qq.com>
2023-02-28 21:52:35 +0800

    Change files

--- code/can.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CAN_H_
-#define _CAN_H_
-
-#include "autosarNM.h"
-#include "Driver_Common.h"
-int MCU_CAN_Transmit(NMPDU_t* NMPDU);
-void MCU_CAN_Init(void);
-
-#endif
--- code/timer.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TIMER_H_
-#define _TIMER_H_
-
-// include "special mcu header"
-
-void TimerInit(void);
-void TimerStop(void);
-
-#endif
